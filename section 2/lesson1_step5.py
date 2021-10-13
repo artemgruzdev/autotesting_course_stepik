cb41554749d619f708d8638264338c1d9b9e2829
aa.gruzdev <aa.gruzdev@tensor.ru>
2021-10-13 16:20:08 +0300

    кликаем по checkboxes и radiobuttons (капча для роботов)

---
+++ section 2/lesson1_step5.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import time
+import math
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+
+browser = webdriver.Chrome()
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser.get(link)
+
+    # Считываем значение переменной x
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    # Вычисляем функцию
+    y = calc(x)
+
+    # Вставляем значение функции в поле ввода
+    answer = browser.find_element_by_id("answer")
+    answer.send_keys(y)
+
+    # Отмечаем чек-бокс
+    checkbox = browser.find_element_by_id("robotCheckbox")
+    checkbox.click()
+
+    # Отмечаем radiobutton
+    radiobutton = browser.find_element_by_id("robotsRule")
+    radiobutton.click()
+
+    # Отправляем заполненную форму
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
